5824c8307cb8d8ea590008c5dcaaad2b6947b95f
kate <kate_utkate@vk.com>
2023-12-28 15:28:29 +0300

    entity installed, data seeded

--- PokemonReviewAPI/Program.cs
+++ PokemonReviewAPI/Program.cs
@@ -1,10 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<Seed>(); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    SeedData(app);
+}
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedDataContext();
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
